bea23eaee25c0d2196386f8ecabc5130a7a6d43f
IsiteYves <yvesisite@gmail.com>
2023-01-21 20:26:52 +0200

    feat: props

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h2>{{ heading }}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    heading: String,
+  },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
